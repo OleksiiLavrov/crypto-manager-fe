43e3ee4c757b6d4833a38ae2dae83f0e49840904
Oleksii Lavrov <lavovalexey@gmail.com>
2024-11-24 19:10:55 +0200

    feat(): remove unused component

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,14 +2,12 @@ import { Route, createBrowserRouter, createRoutesFromElements } from 'react-rout
 import { Dashboard } from '../features/dashboard';
 import { AddTransaction } from '../features/transaction';
 import { App } from '../App';
-import NewDashboard from '../features/new-dashboard';
 
 export const router = createBrowserRouter(
    createRoutesFromElements(
       <Route path="/" element={<App />}>
          <Route index path="/" element={<Dashboard />} />
          <Route path="/dashboard" element={<Dashboard />} />
-         {/* <Route path="/new-dashboard" element={<NewDashboard />} /> */}
          <Route path="/add-transaction" element={<AddTransaction />} />
       </Route>,
    ),
